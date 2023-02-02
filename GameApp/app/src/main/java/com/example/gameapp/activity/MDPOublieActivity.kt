a3c2fc9fb24e4378a07c2cc1fdc74ea49e47ee4c
u21715994 <79019437+u21715994@users.noreply.github.com>
2023-02-02 21:03:14 +0100

    Update MDPOublieActivity.kt

--- GameApp/app/src/main/java/com/example/gameapp/activity/MDPOublieActivity.kt
+++ GameApp/app/src/main/java/com/example/gameapp/activity/MDPOublieActivity.kt
@@ -15,16 +15,40 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
 class MDPOublieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mot_de_passe_oublie)
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            val email = findViewById<EditText>(R.id.editText).text.toString()
-            val auth = UserAuth()
-            auth.resetPassword(email)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MDPOublieFragment())
+            .addToBackStack("mdp_oublie")
+            .commit()
+    }
+
+    class MDPOublieFragment: Fragment(){
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            return LayoutInflater.from(requireContext())
+                .inflate(R.layout.mdp_oublie_fragment, container, false)
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+
+            val button = findViewById<Button>(R.id.button)
+            button.setOnClickListener {
+                val email = findViewById<EditText>(R.id.editText).text.toString()
+                val auth = UserAuth()
+                auth.resetPassword(email)
+            }
         }
     }
 }
